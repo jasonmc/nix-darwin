198ccd9e46d0110318886f79d7e7f0c5aaa3e7d6
Jason McCandless <me@jasonmc.net>
2024-11-23 12:17:08 -0500

    Add home manager config.

--- flake.nix
+++ flake.nix
@@ -3,18 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, }: {
+  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs, }: {
 
     darwinConfigurations = {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#Jasons-MacBook-Pro
       "Jasons-MacBook-Pro" = nix-darwin.lib.darwinSystem {
         system = "aarch64-darwin";
-        modules = [ ./darwin.nix ];
+        modules = [ 
+          ./darwin.nix 
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.jason = import ./home.nix;
+            };
+            users.users.jason.home = "/Users/jason";
+          }
+          ];
         specialArgs = { inherit inputs; };
       };
 
